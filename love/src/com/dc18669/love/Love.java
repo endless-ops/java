9f1032e25392284dc00803174ee1b2a3d9eb7ea4
15083267362 <15083267362@163.com>
2022-11-16 22:52:36 +0800

    跳动的心

--- love/src/com/dc18669/love/Love.java
+++ love/src/com/dc18669/love/Love.java
@@ -1,4 +1,43 @@
 package com.dc18669.love;
 
-public class Love {
+import javax.swing.*;
+
+public class Love extends JFrame {
+
+    private static final double PI = Math.PI;
+
+    public static void main(String[] args) {
+        float x, y, a;
+        for (y = 1.3f; y > -1.1f; y -= 0.06f) {   //这里的参数皆可以调，条件就是看着顺眼
+            for (x = -1.2f; x <= 1.2f; x += 0.025f) {
+                a = x * x + y * y - 1;
+                System.out.print(a * a * a - x * x * y * y * y >= 0.0f ? ' ' : '$');
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println(" ==========================================");
+
+        love();
+    }
+
+    public static void love() {
+        double x, y, a;
+        char s[] = new char[]{'I', ' ', 'l', 'o', 'v', 'e', ' ', 'y', 'o', 'u', '!'};
+        int index = 0;
+        for (y = 1.3f; y > -1.1f; y -= 0.06f) {
+            index = 0;
+            for (x = -1.1f; x <= 1.1f; x += 0.025f) {
+                double result = x * x + Math.pow((5.0 * y / 4.0 - Math.sqrt(Math.abs(x))), 2);
+                if (result <= 1) {
+                    System.out.print((s[index]));
+                    index = (index + 1) % 11;
+                } else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println(" ");
+        }
+    }
 }
